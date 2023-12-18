f446c99423109ca8ee590766cf1d4daaa86cf1a6
ASWINDAS <cvaswin1997@gmail.com>
2023-12-18 09:29:12 +0530

    MULTIPLICATION TABLE

--- python_basics_codewars.py
+++ python_basics_codewars.py
@@ -8,4 +8,9 @@ def is_isogram(string):
 
 print(is_isogram("asdef"))
 
-
+# MULTIPLICATION TABLE:
+def table(n):
+    for i in range(1,11):
+        print(i,"*",n,"=",i*n)
+num=int(input("enter the number : "))
+table(num)
