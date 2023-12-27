4dd46d149fbb2882bcd16a508fab54810c0a8b3f
ASWINDAS <cvaswin1997@gmail.com>
2023-12-27 09:42:08 +0530

    Your code will determine if the number passed is even (or not). The function needs to return either a true or false.

--- python_basics_codewars.py
+++ python_basics_codewars.py
@@ -118,3 +118,15 @@ def distinct(seq):
             seen.add(element)
     
     return result
+
+
+
+
+
+# Your code will determine if the number passed is even (or not).
+# The function needs to return either a true or false.
+def is_even(n): 
+    if n%2 == 0:
+        return True
+    else:
+        return False
