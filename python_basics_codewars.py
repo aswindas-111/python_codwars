54b0fa8d4b9473cf0606200460ebf4269ecc6d71
ASWINDAS <cvaswin1997@gmail.com>
2024-01-22 09:37:45 +0530

    Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love. Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.

--- python_basics_codewars.py
+++ python_basics_codewars.py
@@ -144,3 +144,14 @@ def bin_to_decimal(inp):
 # Return the number (count) of vowels in the given string.
 def get_count(sentence):
     return sum([sentence.count(vowels) for vowels in "aeiou"])
+
+
+# Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+# Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+def lovefunc( flower1, flower2 ):
+    if flower1%2==0 and flower2%2!=0:
+        return True
+    elif flower1%2!=0 and flower2%2==0:
+        return True
+    else:
+        return False
